f3a90ed6ab0713e95d37f83b13da85981ff5ac92
Leon Pahole <leonnieen@gmail.com>
2021-01-03 17:07:32 +0100

    initial commit: spring boot initializer

---
+++ src/main/kotlin/com/leonpahole/nurikabesolverbackend/NurikabeSolverBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.leonpahole.nurikabesolverbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NurikabeSolverBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<NurikabeSolverBackendApplication>(*args)
+}
